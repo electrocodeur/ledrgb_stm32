8cab2a99258b088b84e544b682c595a6e4128519
electrocodeur <77399487+electrocodeur@users.noreply.github.com>
2022-07-13 23:56:32 +0200

    Add files via upload

---
+++ LEDRGB_stm32.ino
@@ -0,0 +1,41 @@
+byte LEDR = PA8;
+byte LEDG = PA9;
+byte LEDB = PA10;
+
+void setup() {
+  pinMode(LEDR, OUTPUT);
+  pinMode(LEDG, OUTPUT);
+  pinMode(LEDB, OUTPUT);
+  couleur(0, 0, 0);
+}
+
+void loop() {
+  couleur(255, 0, 0);
+  delay(1000);
+  couleur(0, 255, 0);
+  delay(1000);
+  couleur(0, 0, 255);
+  delay(1000);
+  couleur(255, 0, 255);
+  delay(1000);
+  couleur(0, 255, 255);
+  delay(1000);
+  couleur(255, 255, 0);
+  delay(1000);
+  couleur(255, 255, 255);
+  delay(1000);
+  couleur(0, 0, 0);
+  delay(1000);
+}
+
+void couleur(byte r, byte g, byte b) {
+
+  // Version cathode commune
+  analogWrite(LEDR, r);
+  analogWrite(LEDG, g);
+  analogWrite(LEDB, b);
+  // Version anode commune
+  /*analogWrite(LEDR, ~r);
+  analogWrite(LEDG, ~g);
+  analogWrite(LEDB, ~b);*/
+}
